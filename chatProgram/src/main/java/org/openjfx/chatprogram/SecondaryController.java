1ee36ff8071d8ea6f335001ad60003cfb97bf718
AJungersen <adam.jungersen@gmail.com>
2021-10-01 16:17:52 +0200

    Commiting the crime of uploading code, that doesnt work

--- chatProgram/src/main/java/org/openjfx/chatprogram/SecondaryController.java
+++ chatProgram/src/main/java/org/openjfx/chatprogram/SecondaryController.java
@@ -2,11 +2,23 @@ package org.openjfx.chatprogram;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.text.TextFlow;
 
 public class SecondaryController {
+@FXML TextField chatBar;
+@FXML TextFlow chatDisplay;
 
-    @FXML
-    private void switchToPrimary() throws IOException {
-        App.setRoot("primary");
-    }
+public void keyPressed(KeyEvent e) throws IOException{
+    input();
+}
+
+@FXML
+private void input() throws IOException{
+
+String chatBarInput = chatBar.getText();
+System.out.println(chatBarInput);
+}
+    
 }
