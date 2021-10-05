797fd62e4a2857e93e7c8b2e19c63b5f583cb8e1
Rawlpluks <claramajse@gmail.com>
2021-10-05 15:18:42 +0200

    UI started, login UI done

--- chatProgram/src/main/java/module-info.java
+++ chatProgram/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.openjfx.chatprogram {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens org.openjfx.chatprogram to javafx.fxml;
     exports org.openjfx.chatprogram;
